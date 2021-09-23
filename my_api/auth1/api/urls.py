94bf527e4712ca9eb99c4a63c1b3dc7204f4a4eb
Decagon <decagon@Decagons-Tobi.local>
2021-09-23 07:52:51 +0100

    logout

--- my_api/auth1/api/urls.py
+++ my_api/auth1/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
-from auth1.api.views import registration_view
+from auth1.api.views import registration_view,logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token,name='login'),
     path('register/', registration_view, name='register'),
-  
+    path('logout/', logout_view, name='logout'),
 ]
