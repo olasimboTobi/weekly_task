4d51a9c9ca2e5a2bc77f615ef5ea392c3f79abb7
Decagon <decagon@Decagons-Tobi.local>
2021-09-23 16:16:36 +0100

    update some files

--- my_api/settings.py
+++ my_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4i!zd8m8it95gh*(06fuxln^%ylpnyw6z-#nqng1v$^17_uv8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
